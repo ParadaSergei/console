0a96b6baad03d3d1d3c2b50012bbfccc71fba4da
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2023-01-11 16:32:51 +0300

    Add files via upload

---
+++ Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Consoles
+{
+    public class Consoles
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите параметр а: ");
+
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Выберете \n" +
+                    "1 |  + \n" +
+                    "2 |  - \n" +
+                    "3 |  / \n" +
+                    "4 |  * \n\n");
+                int c = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите параметр b: ");
+
+                int b = int.Parse(Console.ReadLine());
+
+
+                if (c == 1)
+                {
+                    double otv = a + b;
+                    Console.WriteLine("Ответ :" + otv);
+                }
+                else if (c == 2)
+                {
+                    double otv = a - b;
+                    Console.WriteLine("Ответ :" + otv);
+                }
+                else if (c == 3)
+                {
+                    double otv = a / b;
+                    Console.WriteLine("Ответ :" + otv);
+                }
+                else if (c == 4)
+                {
+                    double otv = a * b;
+                    Console.WriteLine("Ответ :" + otv);
+                }
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine("Неправильно введены данные перезапустите приложения!", e);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f341a53-1f1d-45d1-a9f0-b41e9bdf1ffc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
